787810585f416954c9779b2816a0585365c5f5d0
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2025-03-03 00:28:58 +0530

    frontend api setup and tutorial end

---
+++ app/register/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = res.json();
+      if (!res.ok) {
+        setError("Registration failed");
+      }
+
+      router.push("/login");
+    } catch (error) {
+      console.error("Failed to register", error);
+      setError("Registration failed");
+    }
+  };
+  return <div></div>;
+};
+export default Register;
